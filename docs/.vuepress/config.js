d8b4288b0017a296c808021054c4ce73fb9e0cab
Alan Jui <alanjui.1960@gmail.com>
2022-06-04 09:50:57 +0800

    建置專案版本管理容器

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'VuePress Docs',
+  description: 'Document editting system for Project',
+  base: '/vuepress-starter',
+  themeConfig: {
+    sidebar: [
+      "/ch01/",
+      "/ch02/",
+      "/ch03/",
+      "/ch04/",
+      "/ch05/",
+      "/ch06/",
+    ],
+  }
+}
